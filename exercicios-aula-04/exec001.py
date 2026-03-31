92bc741f29b4a5824244705b4b52a42fffb39bfa
William Berndt Leite <williamb_leite@hotmail.com>
2026-03-31 19:09:23 -0300

    Fix comment formatting in exec001.py

--- exercicios-aula-04/exec001.py
+++ exercicios-aula-04/exec001.py
@@ -3,5 +3,5 @@ nomes = ["Ana", "Pedro", "Matheus", "Gustavo", "William"]
 
 print(nomes[2])
 
-#tempo constante O(1) porque vai diretamente no item que precisa ser buscado
-#independente da quantidade de itens na lista, sem percorrer todo o array
+# constante O(1) porque vai diretamente no item que precisa ser buscado
+# independente da quantidade de itens na lista, sem percorrer todo o array
