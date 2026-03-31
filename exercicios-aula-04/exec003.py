ff448665fafec65a9bc44df2665412dc30324a77
William Berndt Leite <williamb_leite@hotmail.com>
2026-03-31 20:02:10 -0300

    Update exec003.py

--- exercicios-aula-04/exec003.py
+++ exercicios-aula-04/exec003.py
@@ -1,2 +1,2 @@
 
-# porque ao acessar por indice voce vai diretamente no valor que está buscando e nao precisa percorrer o array em busca do item que quer encontrar.
+# porque ao acessar por indice voce vai diretamente no local da memória que ele está alocado e procurar um valor em um array eu preciso ou primeiro ordenar esse array ou percorrer todo o array até encontrar o valor. 
