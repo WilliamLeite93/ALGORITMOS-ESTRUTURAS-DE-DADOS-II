625643ace2ef24913477263147fb8725b30e5faf
William Berndt Leite <williamb_leite@hotmail.com>
2026-03-31 14:14:11 -0300

    Fix comment for clarity in exec003.py

--- exercicios-aula-04/exec003.py
+++ exercicios-aula-04/exec003.py
@@ -1,2 +1,2 @@
 
-# porque ao acessar por indice voce vai diretamente no valor que está buscando e nao precisa percorrer o array para encontrar.
+# porque ao acessar por indice voce vai diretamente no valor que está buscando e nao precisa percorrer o array em busca do item que quer encontrar.
