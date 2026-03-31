705cd673f0a620b098ab0598dfa8409c70005445
William Berndt Leite <williamb_leite@hotmail.com>
2026-03-31 15:20:43 -0300

    Refactor comment for clarity in exec004.py

--- exercicios-aula-04/exec004.py
+++ exercicios-aula-04/exec004.py
@@ -1,2 +1,4 @@
 
-# Quando um novo item é inserido no início de um array, todos os elementos existentes precisam ser deslocados uma posição para a direita para liberar o índice 0 para o novo elemento, Isso faz com que os índices de todos os elementos sejam incrementados em 1 e todo novo elemento passa a ocupar o índice 0.
+# Quando um novo item é inserido no início de um array, todos os elementos existentes precisam ser deslocados
+# uma posição para a direita para liberar o índice 0 para o novo elemento, 
+# Isso faz com que os índices de todos os elementos sejam incrementados em 1 e todo novo elemento passa a ocupar o índice 0.
