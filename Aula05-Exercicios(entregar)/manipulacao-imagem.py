f151e2ff4a0868b9cd2613ba358418158bcfe8e2
William leite <williamb_leite@hotmail.com>
2026-04-20 20:31:53 -0300

    adição do exercicio 4

---
+++ Aula05-Exercicios(entregar)/manipulacao-imagem.py
@@ -0,0 +1,41 @@
+vetor = [50, 100, 150, 200, 210, 220, 230, 240, 250]
+
+def encontrar_Valor(vetor):
+    largura = 3
+    for linha in range(0, 3):
+        for coluna in range(0, 3):
+            indice = linha * largura + coluna
+            valor = vetor[indice]
+            print(f"L:{linha} C:{coluna} -> índice:{indice} valor:{valor}")
+
+encontrar_Valor(vetor)
+
+def filtro_contraste(vetor):
+    for i in range(len(vetor)):
+        valor = vetor[i]
+        novo_valor = round(valor * 1.2)
+
+        if novo_valor > 255:
+            novo_valor = 255
+
+        vetor[i] = novo_valor
+
+filtro_contraste(vetor)
+print()
+print(vetor)
+
+def inversao(vetor):
+  inicio = 0
+  fim = len(vetor) - 1
+
+  while inicio < fim:
+      aux = vetor[inicio]
+      vetor[inicio] = vetor[fim]
+      vetor [fim] = aux
+
+      inicio = inicio + 1
+      fim = fim - 1
+
+inversao(vetor)
+print()
+print(vetor)
